7dacaa2f674b6c5f318fc4fcda327f2f8b8eefcf
Pasindu Charith <65920302+Pc-bytn@users.noreply.github.com>
2023-06-13 00:48:07 +0530

    Sign up and Sign in pages created for student and instructors. Also mapped the related activities to the buttons.

--- app/src/main/java/com/android/pasinducharith/vta_veyangoda/MainActivity.java
+++ app/src/main/java/com/android/pasinducharith/vta_veyangoda/MainActivity.java
@@ -2,13 +2,39 @@ package com.android.pasinducharith.vta_veyangoda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button studentBtn, instructorBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        studentBtn = findViewById(R.id.btn_Student);
+        instructorBtn = findViewById(R.id.btn_Instructor);
+
+        //Call student sign in and up activity when pressed studentBtn
+        studentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_callStudent = new Intent(getApplicationContext(), StudentSignActivity.class);
+                startActivity(intent_callStudent);
+            }
+        });
+
+        //Call instructor sign in and up activity when pressed instructorBtn
+        instructorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_CallInstructor = new Intent(getApplicationContext(), InstructorSignActivity.class);
+                startActivity(intent_CallInstructor);
+            }
+        });
     }
 }
